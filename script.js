b42b5b0abce5b0c073f0531f8fa1b388149ce577
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2025-03-20 22:49:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let i=1;
+let i = 1;
+
 function insert_Row() {
-  const table=document.getElementById("sampleTable");
-	const newRow=table.insertRow(0);
-	const cell1=newRow.insertCell(0);
-	const cell2=newRow.insertCell(1);
-	cell1.innerHTML=`New Cell ${i}`;
-	cell2.innerHTML=`New Cell ${i}`;
-	i++;
+    const table = document.getElementById("sampleTable");
+    const newRow = table.insertRow(0);
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+
+    // Remove the space between "Cell" and the number
+    cell1.innerHTML = `New Cell${i}`;
+    cell2.innerHTML = `New Cell${i}`;
+
+    i++;
 }
