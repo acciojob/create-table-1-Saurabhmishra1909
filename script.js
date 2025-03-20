dd847f524a6663a400752c569701349ba19ee066
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2025-03-20 22:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let i=1;
 function insert_Row() {
   const table=document.getElementById("sampleTable");
 	const newRow=table.insertRow(0);
 	const cell1=newRow.insertCell(0);
 	const cell2=newRow.insertCell(1);
-	cell1.innerHTML="new Cell1";
-	cell2.innerHTML="new Cell2";
+	cell1.innerHTML=`new Cell ${i}`;
+	cell2.innerHTML=`new Cell ${i}`;
+	i++;
 }
